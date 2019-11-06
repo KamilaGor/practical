d9225c3531f6f5907af474f1722988c8d306058c
KamilaGor <kamilagorniak89@gmail.com>
2019-11-06 12:40:45 +0100

    2 commit

---
+++ modul0/src/main/java/com/practical/modul0/Modul0Application.java
@@ -0,0 +1,13 @@
+package com.practical.modul0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Modul0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Modul0Application.class, args);
+	}
+
+}
