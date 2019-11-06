feaf5797984eae37d5c8aa7b786c0e3e84d0ebb9
KamilaGor <kamilagorniak89@gmail.com>
2019-11-06 13:19:46 +0100

    boolean

---
+++ src/com/Main.java
@@ -0,0 +1,13 @@
+package com;
+
+public class Main {
+
+    public static void main(String[] args) {
+        boolean isGoodProgrammer = true;
+        if(isGoodProgrammer) {
+            System.out.println("Jasiek jest świetnym programistą");
+        } else {
+            System.out.println("Jasiek musi się jeszcze dużo nauczyć!");
+        }
+    }
+}
