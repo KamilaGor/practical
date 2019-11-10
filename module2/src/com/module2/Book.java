08e65be044634f4ccce520dbeada592e1e88f35b
KamilaGor <kamilagorniak89@gmail.com>
2019-11-10 22:16:43 +0100

    Part one of module2

---
+++ module2/src/com/module2/Book.java
@@ -0,0 +1,19 @@
+package com.module2;
+
+public class Book {
+	private String name;
+	private int year;
+
+	public Book(String name, int year) {
+		this.name = name;
+		this.year = year;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getYear() {
+		return year;
+	}
+}
--- module2/src/com/module2/Main.java
+++ module2/src/com/module2/Main.java
@@ -1,6 +1,8 @@
 package com.module2;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
 
 public class Main {
 
@@ -69,6 +71,57 @@ public class Main {
         for(int i=0; i<books.size(); i++) {
             System.out.println(books.get(i));
         }
+	    System.out.println();
 
+	    //part4 - LinkedList
+	    System.out.println("Part4: ");
+        LinkedList<Integer> theList = new LinkedList<>();
+        for(int n=0; n<1000; n++) {
+            theList.add(n*2);
+        }
+        System.out.println("List element at 760 index equals: " + theList.get(760));
+        System.out.println("Add new element at 760 index - equals - 566");
+        theList.add(760, 566);
+        System.out.println("I'm checking new element: " + theList.get(760));
+        System.out.println("Size collection: " + theList.size());
+        theList.remove(2);
+        theList.remove();
+        System.out.println("Size collection after removing 2 elements: " + theList.size());
+        System.out.println();
+
+        //part5 - ArrayList
+        System.out.println("Part5: ");
+        ArrayList<Integer> informationTechnology = new ArrayList<>();
+        informationTechnology.add(3);
+        informationTechnology.add(3);
+        informationTechnology.add(4);
+        informationTechnology.add(5);
+        int min = Collections.min(informationTechnology);
+        int max = Collections.max(informationTechnology);
+        System.out.println(min);
+        System.out.println(max);
+        double sum5 = 0;
+        for(int it: informationTechnology) {
+            sum5 += it;
+        }
+        sum5 = sum5 - min - max;
+        double average5 = sum5 / (informationTechnology.size() - 2);
+        System.out.println(average5);
+        System.out.println();
+
+        //part6 - LinkedList
+        System.out.println("Part6: ");
+        LinkedList<Book> list = new LinkedList<>();
+        Book book1 = new Book("BookOne", 1999);
+        Book book2 = new Book("Book2", 2006);
+        Book book3 = new Book("Book3", 2007);
+        list.add(book1);
+        list.add(book2);
+        list.add(book3);
+        for(Book books6: list) {
+            if(books6.getYear() >= 2000) {
+                System.out.println(books6.getName() +  " " +  books6.getYear());
+            }
+        }
     }
 }
